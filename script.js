93e79e631e69930a525899f316cf2b61fac4d315
Eid Hamamda <211023@ppu.edu.ps>
2024-08-11 16:04:01 +0300

    adding functionality

---
+++ script.js
@@ -0,0 +1,61 @@
+"use strict";
+
+//! Page Navigation
+
+//& Event Delegation
+//~ 1. Add event listener to common parent element
+//~ 2. Determine what element originated the event
+document.querySelector(".nav-links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  //~ Matching Strategy
+
+  if (e.target.classList.contains("nav-link")) {
+    const id = e.target.getAttribute("href");
+    // console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+//! Reveal sections
+const allSections = document.querySelectorAll("section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
+
+//! Sticky navigation: Intersection Observer API
+const nav = document.querySelector(".nav");
+const header = document.querySelector(".header");
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
